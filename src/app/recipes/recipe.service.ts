80205dc6d28ba3ee7fbc0de378cfce699b8fc24d
Sushant Raut <raut.sushant@gmail.com>
2020-09-11 16:13:07 -0500

    Saving and feching data on firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,22 +8,29 @@ import { Subject } from 'rxjs';
 export class RecipeService { 
     
   recipesChanged = new Subject<Recipe[]>();
-    private recipes: Recipe[] = [
-        new Recipe('Chicken Tikka', 
-                   'Chicken Tikka Masala', 
-                   'https://www.closetcooking.com/wp-content/uploads/2019/11/Chicken-Tikka-Masala-1200-8412.jpg',
-                   [new Ingredient('Masala', 1),
-                   new Ingredient('Special Masala', 2)
-                ]),
-        new Recipe('Chicken Biryani', 
-                   'Authentic Chicken Biryani', 
-                   'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ0r19KTL77WFPRpEnLSN71pgeYl8jaleep2Q&usqp=CAU',
-                   [new Ingredient('Rice', 1),
-                   new Ingredient('Special Biryani Masala', 2)])
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('Chicken Tikka', 
+    //                'Chicken Tikka Masala', 
+    //                'https://www.closetcooking.com/wp-content/uploads/2019/11/Chicken-Tikka-Masala-1200-8412.jpg',
+    //                [new Ingredient('Masala', 1),
+    //                new Ingredient('Special Masala', 2)
+    //             ]),
+    //     new Recipe('Chicken Biryani', 
+    //                'Authentic Chicken Biryani', 
+    //                'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ0r19KTL77WFPRpEnLSN71pgeYl8jaleep2Q&usqp=CAU',
+    //                [new Ingredient('Rice', 1),
+    //                new Ingredient('Special Biryani Masala', 2)])
+    //   ];
+
+      private recipes: Recipe[] = [];
       
       constructor(private slService: ShoppingListService){
 
+      }
+
+      setRecipes(recipes: Recipe [] ){
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
       }
 
       getRecipes(){
