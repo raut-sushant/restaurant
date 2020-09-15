ec05c0f6127d6b8c699e917bd223250e1c1d610f
Sushant Raut <raut.sushant@gmail.com>
2020-09-15 13:32:35 -0500

    Attaching token with Interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -19,26 +19,20 @@ export class DataStorageService {
     }
 
     fetchRecipes(){
-        return this.authService.user.pipe(
-            take(1), 
-            exhaustMap(user => {
-            return this.http.get<Recipe[]>(
-                'https://ng-course-recipe-book-a5971.firebaseio.com/recipes.json',
-                {
-                    params: new HttpParams().set('auth', user.token)
-                }
-            );        
-        }), 
-        map(recipes => {
-            return recipes.map(recipe => {
-                return {
-                    ...recipe, 
-                    ingredients: recipe.ingredients ? recipe.ingredients : []
-                };
-            });
-        }),        
-        tap(recipes => {
-            this.recipeService.setRecipes(recipes);
-        }));
+        return this.http.get<Recipe[]>(
+                'https://ng-course-recipe-book-a5971.firebaseio.com/recipes.json'
+            ).pipe(
+                map(recipes => {
+                    return recipes.map(recipe => {
+                        return {
+                            ...recipe, 
+                            ingredients: recipe.ingredients ? recipe.ingredients : []
+                        };
+                    });
+                }),        
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
+                })
+            );
     }
 }
