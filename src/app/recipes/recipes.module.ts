cd717ed6a04bed47239577845bcbc190563dd573
Sushant Raut <raut.sushant@gmail.com>
2020-09-15 17:17:01 -0500

    Splitting Recipe Component cleanup

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -21,14 +21,6 @@ import { RecipesRoutingModule } from './recipes-routing.module';
         RecipeStartComponent,
         RecipeEditComponent,
     ],
-    imports: [RouterModule, CommonModule, ReactiveFormsModule, RecipesRoutingModule],
-    exports: [
-        RecipesComponent, 
-        RecipeListComponent, 
-        RecipeItemComponent,
-        RecipeDetailComponent, 
-        RecipeStartComponent,
-        RecipeEditComponent,
-    ]
+    imports: [RouterModule, CommonModule, ReactiveFormsModule, RecipesRoutingModule]
 })
 export class RecipesModule {}
