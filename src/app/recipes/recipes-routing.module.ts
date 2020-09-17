76156083aa9efabe6ef31747d27c720e6dd12f00
Sushant Raut <raut.sushant@gmail.com>
2020-09-17 15:30:35 -0500

    Lazy loading implemented

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -9,7 +9,7 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipesResolverServer } from './recipes-resolver.service';
 
 const routes: Routes = [
-    { path: 'recipes', component: RecipesComponent, 
+    { path: '', component: RecipesComponent, 
     canActivate: [AuthGuard],
     children:[
         { path: '', component: RecipeStartComponent},
