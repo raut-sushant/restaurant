221dacce3986c0c532aedeaa2fcc46f3ca64f083
Sushant Raut <raut.sushant@gmail.com>
2020-09-29 16:46:55 -0500

    added ng universal to preload on server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const appRoutes: Routes = [
     { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
 ];
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
+    imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules, initialNavigation: 'enabled' })],
     exports: [RouterModule]
 })
 export class AppRoutingModule{
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { isPlatformBrowser } from '@angular/common';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
 
@@ -10,10 +11,15 @@ import * as AuthActions from './auth/store/auth.actions';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private store: Store<fromApp.AppState>) {}
+  constructor(
+    private store: Store<fromApp.AppState>, 
+    @Inject(PLATFORM_ID) private platformId
+  ) {}
   
   ngOnInit() {
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if(isPlatformBrowser(this.platformId)){
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
   }
   
 }
